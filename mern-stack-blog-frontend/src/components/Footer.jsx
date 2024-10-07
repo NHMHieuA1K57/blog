85b7c5467b6b99031bbd92ecde32b0154d268a42
DuyTM0508 <duytranls.2003@gmail.com>
2024-10-08 01:30:14 +0700

    UI comment function, add, reply comment

--- mern-stack-blog-frontend/src/components/Footer.jsx
+++ mern-stack-blog-frontend/src/components/Footer.jsx
@@ -91,9 +91,6 @@ const Footer = () => {
             alt="logo"
             className="brightness-0 invert mx-auto md:mx-0"
           />
-          <p className="text-sm text-dark-light text-center mt-4 md:text-left md:text-base lg:text-sm">
-            Build a modern and creative website with crealand
-          </p>
           <ul className="flex justify-center items-center mt-5 space-x-4 text-gray-300 md:justify-start">
             <li>
               <a href="/">
---
+++ mern-stack-blog-frontend/src/pages/articleDetail/ArticleDetailPage.jsx
@@ -0,0 +1,99 @@
+import { Link } from "react-router-dom";
+import BreadCrumbs from "../../components/BreadCrumbs";
+import MainLayout from "../../components/MainLayout";
+import { images } from "../../constants";
+import SuggestedPosts from "./container/SuggestedPosts";
+import SocialShareButtons from "../../components/SocialShareButtons";
+import CommentContainer from "../../components/comments/CommentContainer";
+
+const breadCrumbsData = [
+  { name: "Home", link: "/" },
+  { name: "Blog", link: "/blog" },
+  { name: "Article", link: "/blog/1" },
+];
+
+const posts = [
+  {
+    _id: 1,
+    title: "Help children get better education",
+    images: images.Post1Image,
+    createAt: "2021-09-01",
+  },
+  {
+    _id: 2,
+    title: "Help children get better education",
+    images: images.Post1Image,
+    createAt: "2021-09-01",
+  },
+  {
+    _id: 3,
+    title: "Help children get better education",
+    images: images.Post1Image,
+    createAt: "2021-09-01",
+  },
+  {
+    _id: 4,
+    title: "Help children get better education",
+    images: images.Post1Image,
+    createAt: "2021-09-01",
+  },
+];
+
+const tags = [
+  "Education",
+  "Children",
+  "Better",
+  "Medical",
+  "Health",
+  "Help",
+  "Donation",
+  "Charity",
+];
+
+const ArticleDetailPage = () => {
+  return (
+    <MainLayout>
+      <section className="container mx-auto flex max-w-5xl flex-col px-5 py-5 lg:flex-row lg:items-start lg:gap-x-5">
+        <article className="flex-1">
+          <BreadCrumbs data={breadCrumbsData} />
+          <img
+            className="w-full rounded-xl"
+            src={images.Post1Image}
+            alt="image"
+          />
+          <div className="mt-4 flex gap-2">
+            <Link
+              to={`/blog?category=categoryName`}
+              className="inline-block font-roboto text-sm text-primary md:text-base"
+            >
+              Education
+            </Link>
+          </div>
+          <h1 className="mt-4 font-roboto text-xl font-medium text-dark-hard md:text-[26px]">
+            Help children get better education
+          </h1>
+          <div className="mt-4 text-dark-soft">
+            <p className="leading-7">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae
+              congue mauris rhoncus aenean vel elit scelerisque. In egestas erat
+              imperdiet sed euismod nisi porta lorem mollis. Morbi tristique
+              senectus et netus. Mattis pellentesque id nibh tortor id aliquet
+              lectus proin.
+            </p>
+          </div>
+          <CommentContainer className={"mt-10"} logginedUserId="a"/>
+        </article>
+        <SuggestedPosts
+          header="Latest Article"
+          className="mt-8 lg:mt-0 lg:max-w-xs"
+          posts={posts}
+          tags={tags}
+        />
+      </section>
+    </MainLayout>
+  );
+};
+
+export default ArticleDetailPage;
