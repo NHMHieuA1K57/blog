2d68b0abd9223e8bd4b73c984ecec0cfc84f608e
giakhanh102003 <ngokienhuy550@gmail.com>
2024-10-19 18:08:38 +0700

    Create server.js

---
+++ mern-stack-blog-backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const morgan = require("morgan");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const httpErrors = require("http-errors");
+
+require("dotenv").config();
+
+// khoi tao web server bang express
+const app = express();
+
+// them cac middleware vao web server
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+// thuc hien tiep nhan request tu client bang get
+app.get("/", async (req, res, next) => {
+  res.status(200).json({ message: "Welcome to RESTFul API with NodeJs " });
+});
+
+// them middleware xu ly loi tren : router , Controller , Model
+app.use(async (req, res, next) => {
+  //   next(createError(404,'Không tìm thấy trang '));
+  next(httpErrors.BadRequest("Bad Request "));
+});
+
+ // kiem soat loi bat ki trong web server
+ app.use(async (err, req, res, next) => {
+    //lay status code thuc te dang gap phai
+  res.status(err.status || 500);
+  res.json({
+    status: err.status,
+    message: err.message,
+  });
+  //   res.send(err.message)
+});
+
+const HOST = process.env.HOST_NAME || "localhost";
+const PORT = process.env.PORT || 9999;
+app.listen(PORT, HOST , async() => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
+  });
+  
